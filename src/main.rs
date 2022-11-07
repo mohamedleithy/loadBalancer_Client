b7f598c8806a2caf67f00fcceb2c28d046057041
mohamedleithy <leithy@aucegypt.edu>
2022-11-07 14:12:21 +0200

    4 requests and 4 replies

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io::Read;
 fn main() -> std::io::Result<()>{
     {
         
-        let socket = UdpSocket::bind("172.20.10.5:34255")?;
+        let socket = UdpSocket::bind("10.40.44.255:2022")?;
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
        
@@ -22,12 +22,20 @@ fn main() -> std::io::Result<()>{
 
         f.read(&mut buf)?;
 
-        socket.send_to(&buf, "172.20.10.5:5959")?;
+        socket.send_to(b"req1", "10.40.44.255:2020")?;
+        socket.send_to(b"req2", "10.40.44.255:2020")?;
+        socket.send_to(b"req3", "10.40.44.255:2020")?;
+        socket.send_to(b"req4", "10.40.44.255:2020")?;
         
         
         let (amt, src) = socket.recv_from(&mut buf)?;
-
-        fs::write("reply.txt",&buf)?;
+        fs::write("reply1.txt",&buf)?;
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        fs::write("reply2.txt",&buf)?;
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        fs::write("reply3.txt",&buf)?;
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        fs::write("reply4.txt",&buf)?;
     
     }
 
