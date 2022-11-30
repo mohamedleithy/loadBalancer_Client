2adefea6fc065b67d42233e28e6ed563a994e232
mohamedleithy <leithy@aucegypt.edu>
2022-11-30 15:46:57 +0200

    stats for 100 millis interval

--- clientsScript.py
+++ clientsScript.py
@@ -1,18 +1,19 @@
 import time
 import subprocess
 
-for i in range(0,10):
+for i in range(500,999):
+    
+  
     cmd = (
-            [
-                "cargo",
-                "run",
-                "--", 
-                f'''{i}'''
-            
-            ]
-        )
+                [
+                    "/home/g02-f22/Desktop/loadBalancer_Client/target/debug/loadBalancer_Client", 
+                    f'''{i}''',
+                    "&"
+                
+                ]
+            )
     start = time.time()
-    subprocess.check_call(cmd)
+    subprocess.call(cmd)
     end = time.time()
     elapsed = end - start 
     print(f'''Request {i}: sent: {start} received: {end} elapsed: {elapsed}''')
--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,98 @@
 // importing the udp standard library
 use std::net::UdpSocket;
 use std::fs::File;
-use std::fs;
 use std::io::{Read, Write};
 extern crate local_ip;
-use std::time::{Duration, SystemTime};
+use std::time::{Duration, SystemTime, Instant};
 use std::env;
+use std::io::prelude::*;
+use std::thread;
 
 
 fn main() -> std::io::Result<()>{
     {
+
+      let mut children = vec![];
+      
+      for z in 500 .. 999 {
+
         let args: Vec<String> = env::args().collect();
         
+        let filename = format!("./clientsLog/client{}.txt", z); 
+        let mut file = File::create(filename)?;
 
-
+       children.push( thread::spawn( move || {
         let ip = local_ip::get().unwrap();
-        let port = format!("{}{}",":600", &args[1]);
-        let socket = UdpSocket::bind(ip.to_string() + &port.to_string())?;
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-       
+        let port = format!("{}{}",":9", z);
+        // let socket = UdpSocket::bind(ip.to_string() + &port.to_string()).unwrap();
+        // socket.set_write_timeout(Some(Duration::from_millis(50))).expect("set_write_timeout call failed");
+
+        let msg22 = format!("failed to bind: {}" , port );
+        let socket = UdpSocket::bind(ip.to_string() + &port.to_string()).expect(&msg22);
+
+        let _ = socket.set_read_timeout(Some(Duration::from_millis(50))).expect(&msg22);
+
+        let mut dropped_counter = 0;
+        for n in 1..10000 {
+          thread::sleep(Duration::from_millis(100));
         
-      //  let mut f = File::open("test.text")?;
+          // Receives a single datagram message on the socket. If `buf` is too small to hold
+          // the message, it will be cut off.
+         
+          
+        //  let mut f = File::open("test.text")?;
+  
+          // This is a mutable buffer that will be loaded from the socket
+          // This is a short buffer so any long message will be cut off 
+          let mut buf = [0; 60];
+          
+  
+         // f.read(&mut buf)?;
+      
+      
+          let t = format!("req{}", z);
+          let start = Instant::now();
+          socket.send_to(t.as_bytes(), ip.to_string() +":2020").expect(&msg22);
+  
+      
+          // let (_, _) = socket.recv_from(&mut buf).expect(&msg22);
+          let manga = socket.recv_from(&mut buf);
+          match manga {
+            Ok((_,_src_addr)) => {
+              let duration = start.elapsed();
+  
+              let timeElapsed = format!("time elapsed for request {} client {}: {:?} \n", n , z,duration);
+              file.write_all(timeElapsed.as_bytes()).unwrap();
+              
+            }
+            Err(_) => {
+              dropped_counter = dropped_counter + 1; 
 
-        // This is a mutable buffer that will be loaded from the socket
-        // This is a short buffer so any long message will be cut off 
-        let mut buf = [0; 60];
-        
+            }
+          }
+          
+          
+          let mut msg = [0; 60];
+          msg.copy_from_slice(&buf);
+          msg.reverse();
+          let msg = String::from_utf8((&msg).to_vec()).unwrap();
+          let msg = msg.trim_matches(char::from(0));
+        }
 
-       // f.read(&mut buf)?;
-    
-    
-        let t = format!("req{}", &args[1]);
-        socket.send_to(t.as_bytes(), ip.to_string() +":2020")?;
+        let dropped_msg = format!("{}", dropped_counter);
+        file.write_all(dropped_msg.as_bytes()).unwrap();
+        }
+      ));
+      
+      }
 
 
-    
-        let (_, _) = socket.recv_from(&mut buf)?;
-        
-        let mut msg = [0; 60];
-        msg.copy_from_slice(&buf);
-        msg.reverse();
-        let msg = String::from_utf8((&msg).to_vec()).unwrap();
-        let msg = msg.trim_matches(char::from(0));
-   
+      for child in children{
+
+        let _ = child.join();
+      }
+
+     
     }
 
     Ok(())
