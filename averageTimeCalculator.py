b684963945ae0eb9126b136e10850bf900172dac
abdallahmohamed <abdallah8399@aucegypt.edu>
2022-12-01 10:24:05 +0200

    updating average time script

--- averageTimeCalculator.py
+++ averageTimeCalculator.py
@@ -1,5 +1,12 @@
+import os
+directory = 'logs/test1_clientA'
+
+
+
 accumulator = 0
-numberOfReq = 1000000
+numberOfReq = 0
+milli_counter = 0
+micro_counter = 0
 
 # time elapsed for request 999 client 500: 23.333µs 
 # time elapsed for request 988 client 500: 26.494416ms
@@ -7,7 +14,9 @@ def readFile(filename):
     log = open(filename, 'r')
     lines = log.readlines()
     global accumulator
+    global numberOfReq
     for line in lines[:-1]: 
+        numberOfReq = numberOfReq+1
         time = getTime(line)
         accumulator = accumulator + time
 
@@ -15,17 +24,21 @@ def readFile(filename):
 
 
 def getTime(line):
+    global milli_counter
+    global micro_counter
     
     line = line.split(':')
     line = line[1]
-    unit = line[len(line) - 2]
     line = line.strip()
+    unit = line[len(line) - 2]
     line = line[:-2]
 
+    
     if(unit == 'µ'):
-        print(line)
+        micro_counter = micro_counter + 1
         return float(line)/1000
-    print(line)
+    
+    milli_counter = milli_counter + 1
     return float(line)
 
 
@@ -33,10 +46,17 @@ def getAverage():
 
     average = accumulator/numberOfReq
     print(f'''average request time: {average}''')
+    
+def loopInFolder(folder_name):
+    for filename in os.listdir(directory):
+        if filename != '.DS_Store':
+            readFile(folder_name +filename)
+    
 
 def main(): 
 
-    readFile('/Users/leithy/Desktop/loadBalancer/loadBalancer_Client/logs/test1_clientA/client500.txt')   
+    loopInFolder('logs/test6_clientB/')
+
     #getTime("time elapsed for request 999 client 500: 23.333µs")
     getAverage()
 
